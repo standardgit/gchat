ff04519345dcc5c3f5846a27f8b0c137e12694db
Adejare Toluwalope <standardblaise@gmail.com>
2021-09-25 22:22:50 +0100

    Using devise for authentication.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -65,6 +65,6 @@ class MessagesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def message_params
-      params.require(:message).permit(:title, :content)
+      params.require(:message).permit(:title, :content, :id)
     end
 end
