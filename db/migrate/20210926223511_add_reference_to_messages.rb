a5869a83e0b6937f498aa807c18773dc3786a1e5
standardgit <standardblaise@gmail.com>
2021-10-31 22:30:47 +0100

    Re-modelling the app

--- db/migrate/20210926223511_add_reference_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddReferenceToMessages < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :messages, :conversation, foreign_key: true
-    add_reference :messages, :user, foreign_key: true
-  end
-end
