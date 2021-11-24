8204de97397a6daab681df4623f722de25e5faa9
standardgit <standardblaise@gmail.com>
2021-11-24 21:38:37 +0100

    Adding mesage scaffold.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,3 @@
-const { enviroment } = require('@rails/webpacker')
-const webpack    = require("webpack")
-enviroment.plugins.append("provide", new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-    Popper: ['popper.js', 'default']
-}))
-module.exports = enviroment
+const { environment } = require('@rails/webpacker')
+
+module.exports = environment
--- test/system/messages_test.rb
+++ test/system/messages_test.rb
@@ -14,6 +14,9 @@ class MessagesTest < ApplicationSystemTestCase
     visit messages_url
     click_on "New Message"
 
+    fill_in "Content", with: @message.content
+    fill_in "Room", with: @message.room_id
+    fill_in "User", with: @message.user_id
     click_on "Create Message"
 
     assert_text "Message was successfully created"
@@ -24,6 +27,9 @@ class MessagesTest < ApplicationSystemTestCase
     visit messages_url
     click_on "Edit", match: :first
 
+    fill_in "Content", with: @message.content
+    fill_in "Room", with: @message.room_id
+    fill_in "User", with: @message.user_id
     click_on "Update Message"
 
     assert_text "Message was successfully updated"
