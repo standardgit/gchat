e680a70deb4b8f1f0379091ee6943add05f24d6a
standardgit <standardblaise@gmail.com>
2021-11-24 17:27:42 +0100

    Resorting the app

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @messages = Message.all
-  end
-end
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-
-    validates_uniqueness_of :username
-
-end
