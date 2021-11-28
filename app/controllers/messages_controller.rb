b3153ca9d6671b5b9c8d30e1152ecd6e41bd6bba
standardgit <standardblaise@gmail.com>
2021-11-28 23:38:36 +0100

    Adding temorary username

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,15 +23,10 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
 
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: "Message was successfully created." }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    @message.user = current_user
+
+    @message.save
+    redirect_to request.referer
   end
 
   # PATCH/PUT /messages/1 or /messages/1.json
