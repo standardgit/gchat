175f5d6f64d1918e314fae41be63abfe4fdab3d1
standardgit <standardblaise@gmail.com>
2021-12-06 22:41:31 +0100

    Styling and channeling the rooms

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,28 +1,38 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: room.id }, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id')
+  const room_id = room_element.getAttribute('data-room-id');
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    consumer.subscriptions.remove(subscription)
+  })
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    const element = document.getElementById('user_id')
-    const user_id = Number(element.getAttribute('data-user-id'));
-
-    let html;
-
-    if (user_id === data.message.user_id) {
-      html = data.mine
-    } else {
-      html = data.theirs
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room.id }, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      const user_element = document.getElementById('user_id')
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+  
+      let html;
+  
+      if (user_id === data.message.user_id) {
+        html = data.mine
+      } else {
+        html = data.theirs
+      }
+  
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
     }
+  });  
+})
 
-    const messageContainer = document.getElementById('messages')
-    messageContainer.innerHTML = messageContainer.innerHTML + html
-  }
-});
